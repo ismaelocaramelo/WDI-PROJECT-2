b8405c12a1b9e6ae310b7e6faff181f18162ce77
ismaelocaramelo <ismaelbacha@hotmail.com>
2017-01-18 10:08:53 +0000

    creating heroku app

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/green-charge-vehicle',
-  secret: 'supermega security'
+  db: process.env.MONGODB_URI || 'mongodb://localhost/green-charge-vehicle',
+  secret: process.env.SECRET || 'supermega security'
 };
